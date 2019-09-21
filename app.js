0e90786334b2d3c24ddff0db9a596844d94652bb
Danielle Cushing <dcushing09@gmail.com>
2019-09-21 13:35:42 -0500

    setting up db

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const express = require('express')
+const helmet = require('helmet')
+const bodyParser  = require('body-parser');
+//mpde;s
+const users = require('./models/users');
+const user_logs = require('./models/user_logs');
+
+
 const app = express()
 const port = 3000
+require('dotenv').config()
+// const { Client } = require('pg')
+// const client = new Client()
+// await client.connect()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use(helmet())
+
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', function(req,res,done) {
+	res.json({"message": "hello!"});
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
